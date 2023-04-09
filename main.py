ddf99f64857a08ded4761aa4c1497d67b7c09aa6
Nemanja Veličković <velickovic.nemanja11@gmail.com>
2023-04-09 17:36:47 +0200

    Introduction version final

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from flask_restful import Api, Resource
 app=Flask(__name__)
 api=Api(app)
 
+names= {"tim": {"age": 19, "gender": "male"},
+        "bill": {"age": 70, "gender": "male"},
+        "john": {"age": 25, "gender": "male"}}
+
 class HelloWorld(Resource):
-    def get(self):
+    def get(self,name):
         data = {"data" : "Hello World"}
-        return data.get("data")
+        return names[name]
+    def post(self):
+        return {"data" : "Posted"}
 
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response= requests.get(BASE + "helloworld")
+response= requests.get(BASE + "helloworld/tim")
 print(response.json())
-print(response['data'].json())
